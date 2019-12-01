58dbba193fdba6d093bfe97de3e1b8680d7c3ce9
Jakob Svemar <jakob_svemar@hotmail.com>
2019-12-01 12:08:42 +0100

    refactor code for readability

--- day1/RocketEquation.py
+++ day1/RocketEquation.py
@@ -1,21 +1,24 @@
+def calcCost(weight):
+    return (weight // 3) - 2
+
 def recursiveFuelCost(fuel):
-    extraFuel = (fuel // 3) - 2
+    extraFuel = calcCost(fuel)
     return 0 if extraFuel <= 0 else extraFuel + recursiveFuelCost(extraFuel)
 
 def CalculateFuelCost(moduleArr):
-    moduleTotal = 0
+    modulesTotal = 0
     additionalFuelTotal = 0
     for mass in moduleArr:
-        moduleFuel = (mass // 3) - 2
-        moduleTotal += moduleFuel
+        moduleFuel = calcCost(mass)
+        modulesTotal += moduleFuel
         additionalFuelTotal += recursiveFuelCost(moduleFuel)
-    return moduleTotal, additionalFuelTotal
+    return modulesTotal, additionalFuelTotal
 
 if __name__ == '__main__':
     moduleArr = []
     with open('input.txt') as f:
         for line in f:
             moduleArr.append(int(line))
-    moduleTotal, additionalFuel = CalculateFuelCost(moduleArr)        
-    print('part 1: ' + str(moduleTotal))
-    print('part 2: ' + str(moduleTotal + additionalFuel))
+    modulesTotal, additionalFuel = CalculateFuelCost(moduleArr)        
+    print('part 1: ' + str(modulesTotal))
+    print('part 2: ' + str(modulesTotal + additionalFuel))

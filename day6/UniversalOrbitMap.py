511760b3266c4c151aa6432e46baa274f3e8f1f0
Jakob Svemar <bgm@qlik.com>
2019-12-06 08:06:58 +0100

    remove print comments in day 6

--- day6/UniversalOrbitMap.py
+++ day6/UniversalOrbitMap.py
@@ -19,7 +19,6 @@ def getOrbitSet(val, orbits):
             val = orbits[val]
             orbitSet.add(val)
         else:
-            # print(orbitList)
             return orbitSet
 
 def countOrbitsBetween(start, finish, orbits):
@@ -34,6 +33,5 @@ if __name__ == '__main__':
     with open('input.txt') as f:
         orbits = dict(orb.strip().split(')')[::-1] for orb in f.readlines())
 
-        # print(orbits)
         print("Part 1: %i" % countTotalOrbits(orbits))
         print("Part 2: %i" % countOrbitsBetween("YOU", "SAN", orbits))
